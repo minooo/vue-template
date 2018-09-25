00a43497c7a617b6f4df99badf6c5ad9505cbb90
minooo <13603983223@163.com>
2018-09-25 15:04:32 +0800

    base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,40 @@
 module.exports = {
   // Global CSS
+  mode: 'spa',
   head: {
     title: 'my vue app',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1,user-scalable=no'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: '我的vue, 内容，内容'
-      }
-    ],
-    dev: process.env.NODE_ENV === 'DEV',
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      // {
-      //   rel: 'stylesheet',
-      //   type: 'text/css',
-      //   href: 'https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css'
-      // },
-      // {
-      //   rel: 'stylesheet',
-      //   type: 'text/css',
-      //   href: 'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css'
-      // }
-    ],
-    script: [
-      // {
-      //   src: '//res.wx.qq.com/open/js/jweixin-1.2.0.js'
-      // }
-    ]
+    // meta: [
+    //   { charset: 'utf-8' },
+    //   {
+    //     name: 'viewport',
+    //     content: 'width=device-width, initial-scale=1,user-scalable=no'
+    //   },
+    //   {
+    //     hid: 'description',
+    //     name: 'description',
+    //     content: '我的vue, 内容，内容'
+    //   }
+    // ],
+    // link: [
+    //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    //   {
+    //     rel: 'stylesheet',
+    //     type: 'text/css',
+    //     href: 'https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css'
+    //   },
+    //   {
+    //     rel: 'stylesheet',
+    //     type: 'text/css',
+    //     href: 'https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css'
+    //   }
+    // ],
+    // script: [
+    //   {
+    //     src: '//res.wx.qq.com/open/js/jweixin-1.2.0.js'
+    //   }
+    // ]
   },
-  css: [{ src: '~assets/styles/common.scss', lang: 'scss' }],
+  css: ['~assets/styles/common.css'],
   // 页面切换时的顶部加载条样式定制，也可以直赋值 false 禁用
   loading: false,
   // loading: {
@@ -53,8 +53,8 @@ module.exports = {
       analyzerMode: 'static'
     },
     vendor: ['axios', '~/plugins/vue-notifications'],
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
         // Run ESLint on save
         // config.module.rules.push({
         //   enforce: 'pre',
@@ -63,32 +63,33 @@ module.exports = {
         //   exclude: /(node_modules)/
         // })
         // Extend only webpack config for client-bundle
-        if (ctx.isClient) { config.target = 'electron-renderer' }
       }
-
+      // 注意，开启
+      if (process.env.NODE_ENV === "DEV") { config.target = 'electron-renderer' }
     },
-    loaders: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        loader: 'url-loader',
-        query: {
-          limit: 10000, // 10Kb
-          name: 'img/[name].[hash].[ext]'
-        }
-      }
-    ],
-    postcss: {
-      plugins: {
-        autoprefixer: {
-          browsers: [
-            '> 1%',
-            'last 7 versions',
-            'not ie <= 8',
-            'ios >= 8',
-            'android >= 4.0'
-          ]
-        }
-      }
-    }
-  }
+    // loaders: [
+    //   {
+    //     test: /\.(png|jpe?g|gif|svg)$/,
+    //     loader: 'url-loader',
+    //     query: {
+    //       limit: 10000, // 10Kb
+    //       name: 'img/[name].[hash].[ext]'
+    //     }
+    //   }
+    // ],
+    // postcss: {
+    //   plugins: {
+    //     autoprefixer: {
+    //       browsers: [
+    //         '> 1%',
+    //         'last 7 versions',
+    //         'not ie <= 8',
+    //         'ios >= 8',
+    //         'android >= 4.0'
+    //       ]
+    //     }
+    //   }
+    // }
+  },
+  dev: process.env.NODE_ENV === 'DEV',
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <nuxt-link to="/todos" class="w80 h60 flex ai-center jc-center bg-main font14 c-white">todo page</nuxt-link><br>
     <button class="w60 h20 bg-main mt20 mb20" @click="onBtn">click me</button>
-    <button class="w60 h20 bg-main mt20 mb20" @click="showSuccessMsg">alert !!!</button>
+    <button class="w60 h20 bg-main mt20 mb20" @click="showSuccessMsg">alert123!!</button>
     <div id="con" ref="con" class="box" contenteditable="true" @keyup="message_input">
       <!-- <span class="wait-at" contenteditable="false">hell12o</span>123 -->
     </div>
@@ -61,75 +61,10 @@ export default {
   mounted() {
     console.log(this, "mounted");
     // document.querySelector("#con").addEventListener("paste", this.conListen);
-    document.removeEventListener("paste", this.docListener, false);
-    document.addEventListener("paste", this.docListener, false);
+    // document.removeEventListener("paste", this.docListener, false);
+    // document.addEventListener("paste", this.docListener, false);
   },
   methods: {
-    conListen(e) {
-      if (event.clipboardData || event.originalEvent) {
-        const clipboardData =
-          event.clipboardData || event.originalEvent.clipboardData;
-        if (clipboardData.items) {
-          console.log(
-            "clipboardData.items",
-            clipboardData.items,
-            clipboardData.items.length,
-            clipboardData.items[0]
-          );
-        }
-      }
-    },
-    docListener (e) {
-    var cbd = e.clipboardData || e.originalEvent.clipboardData;;
-
-    var ua = window.navigator.userAgent;
-
-    // // 如果是 Safari 直接 return
-    // if ( !(e.clipboardData && e.clipboardData.items) ) {
-    //     return;
-    // }
-
-    // // Mac平台下Chrome49版本以下 复制Finder中的文件的Bug Hack掉
-    // if(cbd.items && cbd.items.length === 2 && cbd.items[0].kind === "string" && cbd.items[1].kind === "file" &&
-    //     cbd.types && cbd.types.length === 2 && cbd.types[0] === "text/plain" && cbd.types[1] === "Files" &&
-    //     ua.match(/Macintosh/i) && Number(ua.match(/Chrome\/(\d{2})/i)[1]) < 49){
-    //     return;
-    // }
-
-    console.log("cbd", cbd)
-    console.log("cbd.items", cbd.items)
-    const div = document.createElement("div")
-    div.innerText = "hehe"
-    document.body.appendChild(div)
-    for(var i = 0; i < cbd.items.length; i++) {
-        var item = cbd.items[i];
-        console.log("item", item)
-        if(item.kind == "file"){
-            var blob = item.getAsFile();
-            if (blob.size === 0) {
-                return;
-            }
-
-            var reader = new FileReader();
-            reader.onload=function(eve){
-                        //判断文件是否读取完成
-                         if(eve.target.readyState==FileReader.DONE)
-                         {
-            //读取文件完成后，创建img标签来显示服务端传来的字节数//组
-                            var img =document.createElement("img");
-                //读取文件完成后内容放在this===当前
-            //fileReader对象的result属性中，将该内容赋值img标签//浏览器就可以自动解析内容格式并且渲染在浏览器中
-                            img.src=this.result;
-                        //将标签添加到id为show的div中否则，即便是有img也看不见
-                            document.body.appendChild(img);
-                         }
-                     };
-                       reader.readAsDataURL(blob);
-
-
-        }
-    }
-},
     message_input(e) {
       // console.log("con", e, e.code);
     },
